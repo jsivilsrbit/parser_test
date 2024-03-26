2fd6a255569a4d32d04957ec341e4b1242a48b71
jsviilsrbit <joshuasivils@gmail.com>
2024-03-25 22:09:14 -0400

    first try

--- parser.py
+++ parser.py
@@ -1,7 +1,23 @@
 import requests
 
-url = "something google.com"
 
-response = requests.get(url)
+token_url = ""
+client_id = ""
+client_secret = ""
+data = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret
+    }
 
-print(response)
+def get_oauth_token(client_id, client_secret, token_url):
+    response = requests.post(token_url, data=data)
+    if response.status_code == 200:
+        token_data = response.text
+        return token_data.get('access_token')
+    else:
+        print(f"Failed to obtain token, status code: {response.status_code}")
+        return None
+
+token = get_oauth_token(client_id, client_secret, token_url)
+print("Access token:", token)
